e4d5896eeadbb8425cee12449ecf64ebe4b995e1
MarycBrown02 <marycbrown01@sbcgloba.net>
2018-12-06 20:41:14 -0600

    fixed createconnection spelling error

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnecton(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   var connection = mysql.createConnection({
     port: 3306,
